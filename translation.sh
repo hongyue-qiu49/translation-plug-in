43860ac5b9764a24bc46565a51827b93002b1cdc
hongyue-qiu <hongyue.qiu@thoughtworks.com>
2023-10-27 17:52:49 +0800

    feat: allow user input translation source file path

--- translation.sh
+++ translation.sh
@@ -1,10 +1,24 @@
-count=$( jq '.| length' pendingTrans/example.json )
+# cd ~ && cd -
+
+read -p "Please input file path that needs to be translated? " pendingTransPath
+
+pendingTransPath=$([ ! "$pendingTransPath" ] && echo -n 'pendingTrans/example.json' || echo -n $pendingTransPath)
+echo "The file path to be translated is: , $pendingTransPath"
+
+read -p "Please input file path that translation reference file? " translated
+
+translated=$([ ! "$translated" ] && echo -n 'translated/examplezh.json' || echo -n $translated)
+echo "The translation reference file path is: , $translated"
+
+count=$( jq '.| length' $pendingTransPath )
 for i in {0..$((count - 1))}; do
-  valueEn=$(cat pendingTrans/example.json | jq '.[keys['${i}']]')
-  valueTranslated=$(cat translated/examplezh.json | jq '.'${valueEn}'?')
+  valueEn=$(cat $pendingTransPath | jq '.[keys['${i}']]')
+  valueTranslated=$(cat $translated | jq '.'${valueEn}'?')
   if ["null" = "$valueTranslated"]; then
     echo 'not find translation:' + $valueEn
   else
     sed -i '' 's/'$valueEn'/'$valueTranslated'/g'
   fi
 done
+
+echo 'translation finished!'
