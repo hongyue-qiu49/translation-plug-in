a04f5fc6bec5b204943b331ea50e5c3f63876c27
hongyue-qiu <hongyue.qiu@thoughtworks.com>
2023-10-30 15:25:02 +0800

    feat: abstract function and trans simple text

--- translation.sh
+++ translation.sh
@@ -1,23 +1,37 @@
 # cd ~ && cd -
 
-read -p "Please input file path that needs to be translated? " pendingTransPath
+readFilePath() {
+  read -p "${1}" path
 
-pendingTransPath=$([ ! "$pendingTransPath" ] && echo -n 'pendingTrans/example.json' || echo -n $pendingTransPath)
-echo "The file path to be translated is: , $pendingTransPath"
+  path=$([ ! "$path" ] && echo -n "${2}" || echo -n $path)
 
-read -p "Please input file path that translation reference file? " translated
+  echo $path
+}
 
-translated=$([ ! "$translated" ] && echo -n 'translated/examplezh.json' || echo -n $translated)
-echo "The translation reference file path is: , $translated"
+pendingPathInputTip="Please input file path that needs to be translated? "
+pendingDefaultPath="pendingTrans/example.json"
+pendingPathConfirmTip="The file path to be translated is: "
+
+referencePathInputTip="Please input file path that translation reference file? "
+referenceDefaultPath="translated/examplezh.json"
+referencePathConfirmTip="The translation reference file path is: "
+
+pendingTransPath=$(readFilePath "${pendingPathInputTip}" "${pendingDefaultPath}" | tail -1)
+echo $pendingPathConfirmTip $pendingTransPath
+
+translated=$(readFilePath "${referencePathInputTip}" "${referenceDefaultPath}" | tail -1)
+echo $referencePathConfirmTip $translated
 
 count=$( jq '.| length' $pendingTransPath )
-for i in {0..$((count - 1))}; do
+for (( i=0; i < $count; i++ ))
+do
   valueEn=$(cat $pendingTransPath | jq '.[keys['${i}']]')
-  valueTranslated=$(cat $translated | jq '.'${valueEn}'?')
-  if ["null" = "$valueTranslated"]; then
+  valueTranslated=$(cat $translated | jq '.['"${valueEn}"']?')
+  if [ "null" = "$valueTranslated" ]; then
     echo 'not find translation:' + $valueEn
   else
-    sed -i '' 's/'$valueEn'/'$valueTranslated'/g'
+    # todo: need change to regex
+    sed -i '' 's/'": ${valueEn}"'/'": ${valueTranslated}"'/g' $pendingTransPath
   fi
 done
 
